4a5e1c358c1b46511a1163050d601cdf5fc3af5e
Bakary Sylla <syllabakary2002@gmail.com>
2023-09-19 10:24:05 -0700

    browse page updated to fech data

--- src/components/browsePage/BrowsePage.js
+++ src/components/browsePage/BrowsePage.js
@@ -1,14 +1,38 @@
 import "../browsePage/BrowsePage.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const BrowsePage = () => {
+  const [furryList, setFurryList] = useState([]);
+
+  useEffect(() => {
+    // Make an HTTP request to fetch the list of pets
+    fetch("/api/furry")
+      .then((response) => response.json())
+      .then((furryList) => setFurryList(furryList))
+      .catch((error) => console.error("Error fetching pets:", error));
+  }, []);
+
   return (
     <body className="browse-page">
       <main>
-        <div>
+        <div className="browse-title">
           <h1>Welcome to Lost Pet page</h1>
           <h3>Check the list of pet found or lost</h3>
-          <h3>To add, modifie or delete if founded you need to login</h3>
+          <h3>To add, modify or delete if found, you need to login</h3>
+        </div>
+
+        <div className="pet-list">
+          <h2>Pet List:</h2>
+          <ul>
+            {furryList.map((furry) => (
+              <li key={furry.id}>
+                <strong>Name:</strong> {furry.name}
+                <br />
+                <strong>Description:</strong> {furry.furryDescription}
+                {/* Add more pet information here */}
+              </li>
+            ))}
+          </ul>
         </div>
       </main>
     </body>
