06aed7fcb08fb43ec15eba2df32d5acbbac213ad
Bakary Sylla <syllabakary2002@gmail.com>
2023-09-18 22:29:37 -0700

    Login page update with Footer and header

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,10 +1,13 @@
 import React from "react";
 import LoginPage from "../components/login/LoginPage";
-
+import HeaderPage from "../components/header/HeaderPage";
+import Footer from "../components/footer/Footer";
 const Login = () => {
   return (
     <div>
+      <HeaderPage />
       <LoginPage />
+      <Footer />
     </div>
   );
 };
