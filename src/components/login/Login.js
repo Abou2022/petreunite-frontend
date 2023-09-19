e0875fe22f5b6050d105f222a08a2fdd8e5643d6
Bakary Sylla <syllabakary2002@gmail.com>
2023-09-18 22:08:22 -0700

    login components added and css

---
+++ src/components/login/Login.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+function Login() {
+  // State to store user input
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform authentication logic here (e.g., check credentials, make API request)
+
+    // For this example, we'll just log the input values to the console
+    console.log("Email: ", email);
+    console.log("Password: ", password);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
