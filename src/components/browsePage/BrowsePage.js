c184b6b0dd15630e472beeb34d45c44f268d9352
Bakary Sylla <syllabakary2002@gmail.com>
2023-09-22 22:23:24 -0700

    Browse Page updated

--- src/components/browsePage/BrowsePage.js
+++ src/components/browsePage/BrowsePage.js
@@ -33,6 +33,45 @@ const BrowsePage = () => {
     fetchData();
   }, []);
   return (
+    // <body className="browse-page-container">
+    //   <main className="browse-page">
+    //     <section className="browse-title">
+    //       <h1>Welcome to Lost Pet Page</h1>
+    //       <h3>Check the list of pets found or lost</h3>
+    //       <h3>To add, modify, or delete if found, you need to login</h3>
+    //     </section>
+    //     <section className="pet-list">
+    //       <h1>Pet List:</h1>
+    //       <ul>
+    //         {dbFurrys.map((furry) => (
+    //           <li key={furry.id}>
+    //             <strong>Name:</strong> {furry.name}
+    //             <br />
+    //             <strong>Description:</strong> {furry.furryDescription}
+    //             <br />
+    //             <strong>Animal:</strong> {furry.animal}
+    //             <br />
+    //             <strong>Major color:</strong> {furry.color}
+    //             <br />
+    //             <strong>Particluar Sign:</strong> {furry.particularSign}
+    //             <br />
+    //             <strong>Last location:</strong> {furry.lastLocation}
+    //             <br />
+    //             <strong>Pet lost:</strong> {furry.petLost}
+    //             <br />
+    //             <strong>Pet found:</strong> {furry.petFound}
+    //             <br />
+    //             <strong>Pet Picture:</strong> {furry.picture}
+    //             <br />
+    //           </li>
+    //         ))}
+    //       </ul>
+    //     </section>
+    //   </main>
+    // </body>
+
+    //new display check
+
     <body className="browse-page-container">
       <main className="browse-page">
         <section className="browse-title">
@@ -40,34 +79,24 @@ const BrowsePage = () => {
           <h3>Check the list of pets found or lost</h3>
           <h3>To add, modify, or delete if found, you need to login</h3>
         </section>
-        <section className="pet-list">
-          <h1>Pet List:</h1>
-          <ul>
-            {dbFurrys.map((furry) => (
-              <li key={furry.id}>
-                <strong>Name:</strong> {furry.name}
-                <br />
-                <strong>Description:</strong> {furry.furryDescription}
-                <br />
-                <strong>Animal:</strong> {furry.animal}
-                <br />
-                <strong>Major color:</strong> {furry.color}
-                <br />
-                <strong>Particluar Sign:</strong> {furry.particularSign}
-                <br />
-                <strong>Last location:</strong> {furry.lastLocation}
-                <br />
-                <strong>Pet lost:</strong> {furry.petLost}
-                <br />
-                <strong>Pet found:</strong> {furry.petFound}
-                <br />
-                <strong>Pet Picture:</strong> {furry.picture}
-                <br />
-              </li>
-            ))}
-          </ul>
-        </section>
       </main>
+      <div className="project-conatiner">
+        <div className="project-card">
+          {dbFurrys.map((furry) => (
+            <div key={furry.id}>
+              <img src="" alt="" />
+              <h2 className="project-title">Pet Name: {furry.name}</h2>
+              <div className="pro-details">
+                <p>Pet Description : {furry.furryDescription}</p>
+                <p>Animal: {furry.animal}</p>
+                <p> Major Color: {furry.color}</p>
+                <p>Particular Sign: {furry.particularSign}</p>
+                <p>Last Location: {furry.lastLocation}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </body>
   );
 };
