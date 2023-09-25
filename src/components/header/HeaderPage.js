3b1038b906df97c20ef203d16fa0d04a29dee7bc
Bakary Sylla <syllabakary2002@gmail.com>
2023-09-24 22:42:03 -0700

    header components updated

--- src/components/header/HeaderPage.js
+++ src/components/header/HeaderPage.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HeaderPage = () => {
   return (
     <div>
       <header>
-        <h1>Welcome to PetReunite</h1>
+        <Link to="/" className="navbar-logo">
+          {" "}
+          <h1>Welcome to PetReunite</h1>
+        </Link>
         <p>Reuniting Pets with Their Owners</p>
       </header>
     </div>
