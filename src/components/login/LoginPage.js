6e3a4301d338045d6a44c2b6b12f6d6e746c5be7
Bakary Sylla <syllabakary2002@gmail.com>
2023-09-24 20:10:31 -0700

    login updated to fech Data

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -6,21 +6,63 @@ function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  // Function to handle form submission
-  const handleSubmit = (e) => {
+  // Function to handle login
+  const handleLogin = async (e) => {
     e.preventDefault();
-    // Perform authentication logic here (e.g., check credentials, make API request)
 
-    // For this example, we'll just log the input values to the console
-    console.log("Email: ", email);
-    console.log("Password: ", password);
+    try {
+      const response = await fetch("/api/login", {
+        // Replace with your login API endpoint
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Login failed");
+      }
+
+      // Successful login, handle user data or redirect to another page
+      const userData = await response.json();
+      console.log("Logged in user data:", userData);
+    } catch (error) {
+      console.error("Login error:", error);
+    }
+  };
+
+  // Function to handle registration
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/api/register", {
+        // Replace with your registration API endpoint
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Registration failed");
+      }
+
+      // Successful registration, handle user data or redirect to another page
+      const userData = await response.json();
+      console.log("Registered user data:", userData);
+    } catch (error) {
+      console.error("Registration error:", error);
+    }
   };
 
   return (
     <div className="login-wrapper">
       <div className="container">
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit}>
+        <h2>Login or Register</h2>
+        <form onSubmit={handleLogin}>
           <div>
             <label>Email:</label>
             <input
@@ -41,6 +83,30 @@ function LoginPage() {
           </div>
           <button type="submit">Login</button>
         </form>
+        <div>
+          <p>Don't have an account? Register below:</p>
+          <form onSubmit={handleRegister}>
+            <div>
+              <label>Email:</label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label>Password:</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit">Register</button>
+          </form>
+        </div>
       </div>
     </div>
   );
