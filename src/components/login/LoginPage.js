7549f88d32e58bb2f3f1670d8cb097c79f51dee7
Bakary Sylla <syllabakary2002@gmail.com>
2023-10-03 10:38:22 -0700

    loginPage updated with navigate and useState

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,12 +1,32 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const LoginPage = () => {
+  const [emailValue, setEmailValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+
+  const navigate = useNavigate();
+
+  const onLogInClicked = async () => {
+    alter("login is not implemented");
+  };
   return (
     <div className="login-page">
       <h1>Log In</h1>
-      <input type="email" placeholder="someone@gmail.com" />
-      <input type="password" placeholder="password" />
-      <button>Log In</button>
+      <input
+        value={emailValue}
+        onChange={(e) => setEmailValue(e.target.value)}
+        type="email"
+        placeholder="someone@gmail.com"
+      />
+      <input
+        value={passwordValue}
+        onChange={(e) => setPasswordValue(e.target.value)}
+        type="password"
+        placeholder="password"
+      />
+      <button onClick={onLogInClicked}>Log In</button>
       <button>Forgot your password?</button>
       <button>Don't have an account? Sign Up</button>
     </div>
