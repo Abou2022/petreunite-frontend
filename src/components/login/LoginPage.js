c4df4639b973e69bf3c9940d520be5df4a2d77e6
Bakary Sylla <syllabakary2002@gmail.com>
2023-10-03 10:27:32 -0700

    login components reset

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,72 +1,14 @@
-import "../login/LoginPage.css";
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 
-function LoginPage() {
-  // State to store user input
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-  // const url = "http://localhost:3001/api/login";
-
-  // Function to handle login
-  const handleLogin = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch("http://localhost:3001/api/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      });
-
-      if (response.ok) {
-        console.log("Welcome in!");
-
-        // Successful login, handle user data or redirect to another page
-        const userData = await response.json(); // Assuming your user data is in the response JSON
-        console.log("Logged in user data:", userData);
-
-        // Redirect to the browser page
-        navigate("/browse");
-      } else {
-        throw new Error("Login failed");
-      }
-    } catch (error) {
-      console.error("Login error:", error);
-    }
-  };
-
+export const LoginPage = () => {
   return (
-    <div className="login-wrapper">
-      <div className="container">
-        <h2>Login</h2>
-        <form onSubmit={handleLogin}>
-          <div>
-            <label>Email:</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label>Password:</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-      </div>
+    <div className="login-page">
+      <h1>Log In</h1>
+      <input type="email" placeholder="someone@gmail.com" />
+      <input type="password" placeholder="password" />
+      <button>Log In</button>
+      <button>Forgot your password?</button>
+      <button>Don't have an account? Sign Up</button>
     </div>
   );
-}
-
-export default LoginPage;
+};
