2d4a3872663134d2b7db3fc26297a93734b40216
Bakary Sylla <syllabakary2002@gmail.com>
2023-10-03 11:20:10 -0700

    login page updated with more div section

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,4 +1,4 @@
-import "./LoginPage.css";
+import "../login/LoginPage.css";
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -14,31 +14,40 @@ export const LoginPage = () => {
     // alter("login is not implemented");
   };
   return (
-    <div className="login-page">
-      <h1>Log In</h1>
-      {errorMessage && <div className="fail">{errorMessage}</div>}
-      <input
-        value={emailValue}
-        onChange={(e) => setEmailValue(e.target.value)}
-        type="email"
-        placeholder="someone@gmail.com"
-      />
-      <input
-        value={passwordValue}
-        onChange={(e) => setPasswordValue(e.target.value)}
-        type="password"
-        placeholder="password"
-      />
-      <button disabled={!emailValue || !passwordValue} onClick={onLogInClicked}>
-        Log In
-      </button>
-      <button onClick={navigate("/forgot-password")}>
-        {" "}
-        Forgot your password?
-      </button>
-      <button onClick={navigate("/signup")}>
-        Don't have an account? Sign Up
-      </button>
+    <div className="login-container">
+      <div className="login-box">
+        <h1>Log In</h1>
+        {errorMessage && <div className="fail">{errorMessage}</div>}
+        <input
+          className="login-input"
+          value={emailValue}
+          onChange={(e) => setEmailValue(e.target.value)}
+          type="email"
+          placeholder="someone@gmail.com"
+        />
+        <input
+          className="login-input"
+          value={passwordValue}
+          onChange={(e) => setPasswordValue(e.target.value)}
+          type="password"
+          placeholder="password"
+        />
+        <button
+          className="login-button"
+          disabled={!emailValue || !passwordValue}
+          onClick={onLogInClicked}
+        >
+          Log In
+        </button>
+        <div className="login-links">
+          <button onClick={navigate("/forgot-password")}>
+            Forgot your password?
+          </button>
+          <button onClick={navigate("/signup")}>
+            Don't have an account? Sign Up
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
