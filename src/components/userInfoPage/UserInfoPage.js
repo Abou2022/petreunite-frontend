874a583a1689d6bee93c78a840e6f080f49898e8
Bakary Sylla <syllabakary2002@gmail.com>
2023-10-03 22:12:47 -0700

    user info page updated

---
+++ src/components/userInfoPage/UserInfoPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const UserInfoPage = () => {
+  // Your user information rendering logic here
+
+  return (
+    <div className="user-info-container">
+      <h1>User Information</h1>
+      {/* Render user information */}
+    </div>
+  );
+};
+
+export default UserInfoPage;
