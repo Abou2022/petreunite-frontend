ae657211ca99ecc2106c7bdd5ed84d416e092ef3
Bakary Sylla <syllabakary2002@gmail.com>
2023-10-19 11:58:03 -0700

    hooks added in useuser

--- src/components/auth/useUser.js
+++ src/components/auth/useUser.js
@@ -9,4 +9,16 @@ export const useUser = () => {
     const encodedPayload = token.split(".")[1];
     return JSON.parse(atob(encodedPayload));
   };
+  const [user, setUser] = useState(() => {
+    if (!token) return null;
+    return getPayloadFromToken(token);
+  });
+  useEffect(() => {
+    if (!token) {
+      setUser(null);
+    } else {
+      setUser(getPayloadFromToken(token));
+    }
+  }, [token]);
+  return user;
 };
