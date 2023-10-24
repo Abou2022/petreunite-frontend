400351befc68e909f124e3d2716f2387ab6dc307
Bakary Sylla <syllabakary2002@gmail.com>
2023-10-23 22:24:33 -0700

    privateroute updated

--- src/components/auth/PrivateRoute.js
+++ src/components/auth/PrivateRoute.js
@@ -2,20 +2,21 @@ import React from "react";
 import { Navigate, Route } from "react-router-dom";
 import { useUser } from "./useUser";
 
-// export const PrivateRoute = ({ element: Element, ...rest }) => {
-//   const userToken = localStorage.getItem("userToken");
-
-//   if (!userToken) {
-//     return <Navigate to="/login" />;
-//   }
-//   return <Route {...rest} element={<Element />} />;
-// };
-// export default PrivateRoute;
-export const PrivateRoute = (props) => {
+export const PrivateRoute = ({ element: Element, ...rest }) => {
+  // const userToken = localStorage.getItem("userToken");
   const user = useUser();
 
-  if (!user) return <Navigate to="/login" />;
-
-  return <Route {...props} />;
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+  return <Route {...rest} element={<Element />} />;
 };
 export default PrivateRoute;
+// export const PrivateRoute = (props) => {
+//   const user = useUser();
+
+//   if (!user) return <Navigate to="/login" />;
+
+//   return <Route {...props} />;
+// };
+// export default PrivateRoute;
