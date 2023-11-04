3f2072f278bd891017577a5baadff8a55def5563
Bakary Sylla <syllabakary2002@gmail.com>
2023-11-03 22:05:18 -0700

    about content updated

--- src/routes/AboutMe.js
+++ src/routes/AboutMe.js
@@ -1,9 +1,13 @@
 import React from "react";
 import AboutContent from "../components/about/AboutContent";
+import HeaderPage from "../components/header/HeaderPage";
+import Footer from "../components/footer/Footer";
 const AboutMe = () => {
   return (
     <div>
+      <HeaderPage />
       <AboutContent />
+      <Footer />
     </div>
   );
 };
