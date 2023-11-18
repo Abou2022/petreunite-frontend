5d04f3ff3ac357ffbf6fdcc9cbd3cbc35f0ad290
Bakary Sylla <syllabakary2002@gmail.com>
2023-11-17 22:03:08 -0800

    form updated

--- src/components/contact/Form.js
+++ src/components/contact/Form.js
@@ -1,68 +1,69 @@
 import "../contact/FormStyles.css";
-import React from "react";
+import React, { useState } from "react";
 import { useRef } from "react";
 import emailjs from "@emailjs/browser";
 
 const Form = () => {
   const form = useRef();
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const sendEmail = (e) => {
+  const sendEmail = async (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
+    try {
+      const result = await emailjs.sendForm(
         "service_ueg4ybw",
         "template_d9mi09m",
         form.current,
         "4r_t6hl2yML6GWN5o"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
       );
+
+      console.log(result.text);
+      setIsSubmitted(true);
+    } catch (error) {
+      console.log(error.text);
+    }
+
     e.target.reset();
   };
-
+  if (sendEmail) {
+    alert("Thank you");
+  }
   return (
     <div className="contact-container">
       <div className="form">
-        <form ref={form} onSubmit={sendEmail}>
-          <label>Your name</label>
-          <input
-            type="text"
-            name="user_name"
-            required
-            placeholder="Required"
-          ></input>
-          <label>Email</label>
-          <input
-            type="email"
-            name="user_email"
-            required
-            placeholder="Required"
-          ></input>
-          <label>Subject</label>
-          <input
-            type="text"
-            name="subject"
-            required
-            placeholder="Required"
-          ></input>
-          <label>Message</label>
-          <textarea
-            rows="6"
-            placeholder="Type your message here (Required)"
-            name="message"
-            required
-          />
-          <button type="submit" className="btn">
-            Submit
-          </button>
-        </form>
+        {isSubmitted ? (
+          <p>Thank you for contacting me!</p>
+        ) : (
+          <form ref={form} onSubmit={sendEmail}>
+            <label>Your name</label>
+            <input
+              type="text"
+              name="user_name"
+              required
+              placeholder="Required"
+            />
+            <label>Email</label>
+            <input
+              type="email"
+              name="user_email"
+              required
+              placeholder="Required"
+            />
+            <label>Subject</label>
+            <input type="text" name="subject" required placeholder="Required" />
+            <label>Message</label>
+            <textarea
+              rows="6"
+              placeholder="Type your message here (Required)"
+              name="message"
+              required
+            />
+            <button type="submit" className="btn">
+              Submit
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );
